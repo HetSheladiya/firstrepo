a4bae1dddedf7db681a7d7981598caba1b1c5e37
HetSheladiya <sheladih@sheridancollege.ca>
2024-09-16 12:49:43 -0400

    initial commit i added comment

---
+++ src/javaapplication1mondayclass/JavaApplication1MondayClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1mondayclass;
+
+/**
+ *
+ * @author Het Sheladiya
+ * @modifiedby Het Sheladiya
+ * hii there
+ */
+public class JavaApplication1MondayClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
